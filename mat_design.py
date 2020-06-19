39a153b5dd1b81da69217c72f28fab8c540ef319
sam1bit <66423015+sam1bit@users.noreply.github.com>
2020-06-19 20:24:53 +0530

    Update mat_design.py

--- mat_design.py
+++ mat_design.py
@@ -1,19 +1,4 @@
-"""
-n=7
-m=21
 
-for i in range(n//2):
-    a=1+i*2
-    print((".|."*a).center(m,"-"))
-print("WELCOME".center(m,"-"))
-for i in range(n//2):
-      print((".|."*a).center(m,"-"))
-      a=a-2
- 
-n, m = map(int,input().split())
-pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
-print()"""
-####################################
 n, m = map(int,input().split())
 alist=[]
 for i in range(n//2):
